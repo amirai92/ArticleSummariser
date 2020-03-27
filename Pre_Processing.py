2728363987c6b478934996f58a78e3d5bf72b2dd
Amir <amiraizin20@gmail.com>
2020-03-28 02:28:45 +0300

    Adding a Contraction Mapping class

--- Pre_Processing.py
+++ Pre_Processing.py
@@ -16,7 +16,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 import warnings
 from keras import backend as K
 from Attention import AttentionLayer
-
+from Contraction_Mapping import Contraction_Mapping
 K.clear_session()
 latent_dim = 500
 from matplotlib import pyplot
@@ -26,80 +26,26 @@ warnings.filterwarnings("ignore")
 
 
 
-contraction_mapping = {"ain't": "is not", "aren't": "are not", "can't": "cannot", "'cause": "because",
-                       "could've": "could have", "couldn't": "could not",
 
-                       "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not",
-                       "hasn't": "has not", "haven't": "have not",
+#Init a object of ContractionMapping
+p1=Contraction_Mapping()
+contraction_mapping=p1.contraction_mapping
 
-                       "he'd": "he would", "he'll": "he will", "he's": "he is", "how'd": "how did",
-                       "how'd'y": "how do you", "how'll": "how will", "how's": "how is",
-
-                       "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have",
-                       "I'm": "I am", "I've": "I have", "i'd": "i would",
-
-                       "i'd've": "i would have", "i'll": "i will", "i'll've": "i will have", "i'm": "i am",
-                       "i've": "i have", "isn't": "is not", "it'd": "it would",
-
-                       "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have", "it's": "it is",
-                       "let's": "let us", "ma'am": "madam",
-
-                       "mayn't": "may not", "might've": "might have", "mightn't": "might not",
-                       "mightn't've": "might not have", "must've": "must have",
-
-                       "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not",
-                       "needn't've": "need not have", "o'clock": "of the clock",
-
-                       "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not",
-                       "sha'n't": "shall not", "shan't've": "shall not have",
-
-                       "she'd": "she would", "she'd've": "she would have", "she'll": "she will",
-                       "she'll've": "she will have", "she's": "she is",
-
-                       "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have",
-                       "so've": "so have", "so's": "so as",
-
-                       "this's": "this is", "that'd": "that would", "that'd've": "that would have", "that's": "that is",
-                       "there'd": "there would",
-
-                       "there'd've": "there would have", "there's": "there is", "here's": "here is",
-                       "they'd": "they would", "they'd've": "they would have",
-
-                       "they'll": "they will", "they'll've": "they will have", "they're": "they are",
-                       "they've": "they have", "to've": "to have",
-
-                       "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will",
-                       "we'll've": "we will have", "we're": "we are",
-
-                       "we've": "we have", "weren't": "were not", "what'll": "what will",
-                       "what'll've": "what will have", "what're": "what are",
-
-                       "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have",
-                       "where'd": "where did", "where's": "where is",
-
-                       "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is",
-                       "who've": "who have",
-
-                       "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not",
-                       "won't've": "will not have",
-
-                       "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have",
-                       "y'all": "you all",
-
-                       "y'all'd": "you all would", "y'all'd've": "you all would have", "y'all're": "you all are",
-                       "y'all've": "you all have",
-
-                       "you'd": "you would", "you'd've": "you would have", "you'll": "you will",
-                       "you'll've": "you will have",
-
-                       "you're": "you are", "you've": "you have"}
 
 # Reading The Data out:
 data = pd.read_csv('/Users/Amir/PycharmProjects/FinalProject/amazon-fine-food-reviews/Reviews.csv', nrows=10000)
 
-""" Clean function : 
-Convert everything to lowercase , Remove HTML tags ,Contraction mapping ,Remove (‘s) , 
-Remove any text inside the parenthesis ( ) , Eliminate punctuations and special characters,Remove stopwords,Remove short words.
+#Drop Duplicates and NA values functions:
+data.drop_duplicates(subset=['Text'],inplace=True)  #dropping duplicates
+data.dropna(axis=0,inplace=True)   #dropping na
+
+
+
+""" Clean function , doing the following things: 
+Convert everything to lowercase , Remove HTML tags ,Contraction mapping ,Remove (‘s)
+,Remove any text inside the parenthesis ( ) 
+,Eliminate punctuations and special characters,Remove stopwords
+,Remove short words.
 """
 
 stop_words = set(stopwords.words('english'))
@@ -157,7 +103,8 @@ data['cleaned_summary'] = data['cleaned_summary'].apply(lambda x: '_START_ ' + x
 """
 Understanding the distribution of the sequences
 Here, we will analyze the length of the reviews and the summary to get an overall idea about the distribution 
-    of length of the text. This will help us fix the maximum length of the sequence:
+of length of the text. 
+This will help us fix the maximum length of the sequence:
 """
 
 text_word_count = []
@@ -172,21 +119,19 @@ for i in data['cleaned_summary']:
 
 length_df = pd.DataFrame({'text': text_word_count, 'summary': summary_word_count})
 length_df.hist(bins=30)
+#Plt Show :
 #plt.show()
 
 max_len_text = 100
 max_len_summary = 10
-# split our dataset into a training and validation set. We’ll use 90% of the dataset as the training data and evaluate the performance on the remaining 10% (holdout set):
+# spliting the dataset into a training and validation set. We’ll use 90% of the dataset as the training data and evaluate the performance on the remaining 10% (holdout set):
 x_tr, x_val, y_tr, y_val = train_test_split(data['cleaned_text'], data['cleaned_summary'], test_size=0.1, random_state=0, shuffle=True)
 
 
 """
 Preparing the Tokenizer
-
-A tokenizer builds the vocabulary and converts a word sequence to an integer sequence. Go ahead and build tokenizers for text and summary:
-
+A tokenizer builds the vocabulary and converts a word sequence to an integer sequence.
 """
-print('Tokenizer')
 # Text Tokenizer
 
 # prepare a tokenizer for reviews on training data
@@ -218,7 +163,6 @@ y_tr = pad_sequences(y_tr, maxlen=max_len_summary, padding='post')
 y_val = pad_sequences(y_val, maxlen=max_len_summary, padding='post')
 
 y_voc_size = len(y_tokenizer.word_index) + 1
-print('Before Model Building')
 
 
 #Model Building:
