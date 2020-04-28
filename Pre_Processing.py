3e046bbf5ff088e2c1324101277e23df216638f3
Amir <amiraizin20@gmail.com>
2020-04-28 18:40:26 +0300

    Fixing Infernce layer

--- Pre_Processing.py
+++ Pre_Processing.py
@@ -1,5 +1,6 @@
 # Author Amir Aizin
 import os
+from keras.callbacks import TensorBoard, ModelCheckpoint
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ from bs4 import BeautifulSoup
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from nltk.corpus import stopwords
-import matplotlib.pyplot as plt
+from matplotlib import pyplot
 from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, TimeDistributed, Bidirectional
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
@@ -18,57 +19,53 @@ from Attention import AttentionLayer
 from Contraction_Mapping import Contraction_Mapping
 K.clear_session()
 latent_dim = 500
-from matplotlib import pyplot
-
 pd.set_option("display.max_colwidth", 200)
 warnings.filterwarnings("ignore")
 
 
 
-
-#Init a object of Contraction Mapping
-p1=Contraction_Mapping()
-contraction_mapping=p1.contraction_mapping
-
+# Init a object of Contraction Mapping
+p1 = Contraction_Mapping()
+contraction_mapping = p1.contraction_mapping
 
 # Reading The Data out:
 dataT = []
 dataS = []
-PathText="/Users/Amir/PycharmProjects/FinalProject/DS/TXT"
-PathSumarry="/Users/Amir/PycharmProjects/FinalProject/DS/SUM"
+PathText = "/Users/Amir/PycharmProjects/FinalProject/DS/TXT"
+PathSumarry = "/Users/Amir/PycharmProjects/FinalProject/DS/SUM"
 
 
 def readFile():
     for file in os.listdir(PathText):
-        print(file)
         full_path = os.path.join(PathText, file)
         if os.path.isfile(full_path):
-            with open(full_path,"r",encoding="utf8") as f:
+            with open(full_path, "r", encoding="utf8") as f:
                 dataT.append(f.read())
 
     for file in os.listdir(PathSumarry):
 
         full_path = os.path.join(PathSumarry, file)
         if os.path.isfile(full_path):
-            with open(full_path,"r",encoding="utf8") as f:
-                            dataS.append(f.read())
+            with open(full_path, "r", encoding="utf8") as f:
+                dataS.append(f.read())
 
 
-
-#Calling the above function for reading the files from the directory
+# Calling the above function for reading the files from the directory
 readFile()
 
-data = pd.DataFrame(zip(dataT,dataS), columns=['Text','Summary'])
+#Create Pandas dataframe
+data = pd.DataFrame(zip(dataT, dataS), columns=['Text', 'Summary'])
 
+
+#TODO:Remove this
 #Printing the the CSV File.
-for row in data.iterrows():
-    print (row)
-
-#Drop Duplicates and NA values functions:
-data.drop_duplicates(subset=['Text'],inplace=True)  #dropping duplicates
-data.dropna(axis=0,inplace=True)   #dropping na
+#for row in data.iterrows():
+#    print (row)
 
 
+# Drop Duplicates and NA values functions:
+data.drop_duplicates(subset=['Text'], inplace=True)  # dropping duplicates
+data.dropna(axis=0, inplace=True)  # dropping na
 
 """ Clean function , doing the following things: 
 Convert everything to lowercase , Remove HTML tags ,Contraction mapping ,Remove (‘s),
@@ -77,10 +74,10 @@ Eliminate punctuations and special characters,Remove stopwords ,
 Remove short words.
 """
 
-
 stop_words = set(stopwords.words('english'))
 
-#Text Cleaning
+
+# Text Cleaning
 def text_cleaner(text):
     newString = text.lower()
     newString = BeautifulSoup(newString, "lxml").text
@@ -127,8 +124,12 @@ data['cleaned_summary'] = cleaned_summary
 data['cleaned_summary'].replace('', np.nan, inplace=True)
 data.dropna(axis=0, inplace=True)
 
-#The START and END special tokens at the beginning and end of the summary:
+
+
+# The START and END special tokens at the beginning and end of the summary:
 data['cleaned_summary'] = data['cleaned_summary'].apply(lambda x: '_START_ ' + x + ' _END_')
+
+
 
 """
 Understanding the distribution of the sequences.
@@ -150,22 +151,31 @@ for i in data['cleaned_summary']:
 length_df = pd.DataFrame({'text': text_word_count, 'summary': summary_word_count})
 length_df.hist(bins=30)
 
-#TODO: remove this
-#Plt Show :
-#plt.show()
+# TODO: remove this
+#pyplot.show()
 
-#This variables can be changed:
-max_len_text = 100
-max_len_summary = 10
+#Check the biggest file size :
+#print("Text",data["Text"].str.len().max())
+#print("Sum", data["Summary"].str.len().max())
+
+
+# This variables can be changed depends on the plot that printed before or by finding the maximum length of summary and text
+max_len_text = 12000
+max_len_summary = 500
+
 
 # spliting the dataset into a training and validation set. We’ll use 90% of the dataset as the training data and evaluate the performance on the remaining 10% (holdout set):
-x_tr, x_val, y_tr, y_val = train_test_split(data['cleaned_text'], data['cleaned_summary'], test_size=0.1, random_state=0, shuffle=True)
+x_tr, x_val, y_tr, y_val = train_test_split(data['cleaned_text'], data['cleaned_summary'], test_size=0.1,
+                                            random_state=0, shuffle=True)
 
 
-"""
-Preparing the Tokenizer
-A tokenizer builds the vocabulary and converts a word sequence to an integer sequence.
-"""
+
+
+
+
+#Preparing the Tokenizer
+#           A tokenizer builds the vocabulary and converts a word sequence to an integer sequence.
+
 # Text Tokenizer
 
 # prepare a tokenizer for reviews on training data
@@ -198,67 +208,174 @@ y_val = pad_sequences(y_val, maxlen=max_len_summary, padding='post')
 
 y_voc_size = len(y_tokenizer.word_index) + 1
 
-
-#Model Building:
+# Model Building:
 
 
 # Encoder
 encoder_inputs = Input(shape=(max_len_text,))
-enc_emb = Embedding(x_voc_size, latent_dim,trainable=True)(encoder_inputs)
+enc_emb = Embedding(x_voc_size, latent_dim, trainable=True)(encoder_inputs)
 
-#LSTM 1
-encoder_lstm1 = LSTM(latent_dim,return_sequences=True,return_state=True)
+# LSTM 1
+encoder_lstm1 = LSTM(latent_dim, return_sequences=True, return_state=True)
 encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
 
-#LSTM 2
-encoder_lstm2 = LSTM(latent_dim,return_sequences=True,return_state=True)
+# LSTM 2
+encoder_lstm2 = LSTM(latent_dim, return_sequences=True, return_state=True)
 encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
 
-#LSTM 3
-encoder_lstm3=LSTM(latent_dim, return_state=True, return_sequences=True)
-encoder_outputs, state_h, state_c= encoder_lstm3(encoder_output2)
+# LSTM 3
+encoder_lstm3 = LSTM(latent_dim, return_state=True, return_sequences=True)
+encoder_outputs, state_h, state_c = encoder_lstm3(encoder_output2)
 
 # Set up the decoder.
 decoder_inputs = Input(shape=(None,))
-dec_emb_layer = Embedding(y_voc_size, latent_dim,trainable=True)
+dec_emb_layer = Embedding(y_voc_size, latent_dim, trainable=True)
 dec_emb = dec_emb_layer(decoder_inputs)
 
-#LSTM using encoder_states as initial state
+# LSTM using encoder_states as initial state
 decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs,decoder_fwd_state, decoder_back_state = decoder_lstm(dec_emb,initial_state=[state_h, state_c])
+decoder_outputs, decoder_fwd_state, decoder_back_state = decoder_lstm(dec_emb, initial_state=[state_h, state_c])
 
-#Attention Layer
+# Attention Layer
 attn_layer = AttentionLayer(name='attention_layer')
 attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
 
 # Concat attention output and decoder LSTM output
 decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attn_out])
 
-#Dense layer
+# Dense layer
 decoder_dense = TimeDistributed(Dense(y_voc_size, activation='softmax'))
 decoder_outputs = decoder_dense(decoder_concat_input)
-
-
-
-
-
 
 # Define the model
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 model.summary()
 
+
+#TODO: check where I can add Learning rate:
+
 # Run training
-model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2)
 
-#history attribute is a record of training loss values and metrics values at successive epochs.
-history=model.fit([x_tr,y_tr[:,:-1]], y_tr.reshape(y_tr.shape[0],y_tr.shape[1], 1)[:,1:] ,epochs=50,callbacks=[es],batch_size=512, validation_data=([x_val,y_val[:,:-1]], y_val.reshape(y_val.shape[0],y_val.shape[1], 1)[:,1:]))
-# 
+
+#change from test1.h5 the name of the model give instead
+
+checkpoint = ModelCheckpoint(filepath="weights.best.hdf5", monitor='val_acc', verbose=1, save_best_only=True,
+                             save_weights_only=True, mode='auto', period=1)
+
+
+#tensorboard = TensorBoard(log_dir="model", histogram_freq=1, write_graph=True, write_images=True)
+# history attribute is a record of training loss values and metrics values at successive epochs.
+
+history = model.fit([x_tr, y_tr[:, :-1]], y_tr.reshape(y_tr.shape[0], y_tr.shape[1], 1)[:, 1:], epochs=32,
+                    callbacks=[es,checkpoint], batch_size=128,
+                    validation_data=([x_val, y_val[:, :-1]], y_val.reshape(y_val.shape[0], y_val.shape[1], 1)[:, 1:]))
+
+
 
 pyplot.plot(history.history['loss'], label='train')
+
 pyplot.plot(history.history['val_loss'], label='test')
+
 pyplot.legend()
-pyplot.show()
+
+#TODO: Remove this :
+#pyplot.show()
 
 
+reverse_target_word_index = y_tokenizer.index_word
+reverse_source_word_index = x_tokenizer.index_word
+target_word_index = y_tokenizer.word_index
+
+# Infernce
+# encoder inference
+encoder_model = Model(inputs=encoder_inputs, outputs=[encoder_outputs, state_h, state_c])
+
+# decoder inference
+# Below tensors will hold the states of the previous time step
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_hidden_state_input = Input(shape=(max_len_text, latent_dim))
+
+# Get the embeddings of the decoder sequence
+dec_emb2 = dec_emb_layer(decoder_inputs)
+
+# To predict the next word in the sequence,we set the initial states to the states from the previous time step
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2,
+                                                   initial_state=[decoder_state_input_h, decoder_state_input_c])
+
+
+# attention inference
+attn_out_inf, attn_states_inf = attn_layer([decoder_hidden_state_input, decoder_outputs2])
+decoder_inf_concat = Concatenate(axis=-1, name='concat')([decoder_outputs2, attn_out_inf])
+
+# A dense softmax layer to generate prob dist. over the target vocabulary
+decoder_outputs2 = decoder_dense(decoder_inf_concat)
+
+# Final decoder model
+decoder_model = Model(
+    [decoder_inputs] + [decoder_hidden_state_input, decoder_state_input_h, decoder_state_input_c],
+    [decoder_outputs2] + [state_h2, state_c2])
+
+
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    e_out, e_h, e_c = encoder_model.predict(input_seq)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1))
+
+    # Choose the 'start' word as the first word of the target sequence
+    target_seq[0, 0] = target_word_index['start']
+
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + [e_out, e_h, e_c])
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_token = reverse_target_word_index[sampled_token_index]
+
+        if (sampled_token != 'end'):
+            decoded_sentence += ' ' + sampled_token
+
+            # Exit condition: either hit max length or find stop word.
+            if sampled_token == 'end' or len(decoded_sentence.split()) >= (max_len_summary - 1):
+                stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = sampled_token_index
+
+        # Update internal states
+        e_h, e_c = h, c
+
+    return decoded_sentence
+
+
+def seq2summary(input_seq):
+    newString = ''
+    for i in input_seq:
+        if ((i != 0 and i != target_word_index['start']) and i != target_word_index['end']):
+            newString = newString + reverse_target_word_index[i] + ' '
+    return newString
+
+
+def seq2text(input_seq):
+    newString = ''
+    for i in input_seq:
+        if (i != 0):
+            newString = newString + reverse_source_word_index[i] + ' '
+    return newString
+
+
+
+
+for i in range(len(x_val)):
+    print("Review:", seq2text(x_val[i]))
+    print("Original summary:", seq2summary(y_val[i]))
+    print("Predicted summary:", decode_sequence(x_val[i].reshape(1, max_len_text)))
+    print("\n")
