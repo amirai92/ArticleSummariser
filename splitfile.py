ed34cb486d301e73243c32f8c770941d2a4d500d
Amir <amiraizin20@gmail.com>
2020-06-16 19:14:53 +0300

    changed the format name of the files .

--- splitfile.py
+++ splitfile.py
@@ -1,53 +1,58 @@
 from datetime import datetime
 import logging
 import os
-
 import re
+
 pathsections = "DataSet/Sections"
-text=[]
+text = []
+save_file_name = []
 
 """
-
-        doc_splitter = re.compile(r"^(?:Section\ )?\d+[\.\d+]?", re.MULTILINE)
-       starts = [match.span()[0] for match in doc_splitter.finditer(listToStr)] + [len(listToStr)]
-       sections = [listToStr[starts[idx]:starts[idx + 1]] for idx in range(len(starts) - 1)]
+doc_splitter = re.compile(r"^(?:Section\ )?\d+[\.\d+]?", re.MULTILINE)
+starts = [match.span()[0] for match in doc_splitter.finditer(listToStr)] + [len(listToStr)]
+sections = [listToStr[starts[idx]:starts[idx + 1]] for idx in range(len(starts) - 1)]      
 """
 
 
 def readData():
+    # split by regular expression
     doc_splitter = re.compile(r"^(?:Section\ )?\d+[\.\d+]?", re.MULTILINE)
-
-
-
     for file in os.listdir(pathsections):
         full_path = os.path.join(pathsections, file)
         if os.path.isfile(full_path):
             with open(full_path, "r", encoding="utf-8") as f:
-                sections= []
-                text= []
-                listToStr= ""
+                sections = []
+                text = []
+                listToStr = ""
+
                 text.append((f.read()))
                 listToStr = ' '.join([str(elem) for elem in text])
-
-
                 starts = [match.span()[0] for match in doc_splitter.finditer(listToStr)] + [len(listToStr)]
                 sections = [listToStr[starts[idx]:starts[idx + 1]] for idx in range(len(starts) - 1)]
-
                 for i, name in enumerate(sections):
-                    f = open(file + str(i + 1) + ".txt", "w",encoding='utf-8')
-
+                    split_file = file.split(sep='.')[0]
+                    split_file = split_file + str(i + 1) + ".txt"
+                    PathSections = os.path.join(pathsections, split_file)
+                    f = open(PathSections , "w", encoding='utf-8')
                     f.write(name + "\n")
                     f.close()
 
-                #PathSections = os.path.join(pathsections, file)
-                #oldfile = open(PathSections, 'w', encoding="utf-8")
-                #oldfile.write(sections)
 
+def try_1(save_file_name=None):
+    for file in os.listdir(pathsections):
+        full_path = os.path.join(pathsections, file)
+        if os.path.isfile(full_path):
+            with open(full_path, "r", encoding="utf-8") as f:
+                save_file_name.append((f.readline()))
+                f.seek(0)
+    save_file_name.sort()
 
 
 
 if __name__ == "__main__":
     readData()
+   # try_1(save_file_name)
+
 
 
 
